90758d31417b8bb09af9f5f171d97892d1e347d9
root <root@innovotech-n1.(none)>
2014-01-03 01:06:10 +0800

    modify uwsgi_django and add uwsgi log dir

--- supervisor_django/attributes/default.rb
+++ supervisor_django/attributes/default.rb
@@ -1,3 +1,4 @@
 default['supervisor_django']['uwsgi']['binary'] = '/usr/local/bin/uwsgi'
 default['supervisor_django']['uwsgi']['wrapper'] = '/usr/local/bin/uwsgi_django.sh'
+default['supervisor_django']['uwsgi']['logdir'] = '/var/log/uwsgi'
 default['supervisor_django']['uwsgi']['version'] = '1.4.9'
--- supervisor_django/recipes/default.rb
+++ supervisor_django/recipes/default.rb
@@ -8,6 +8,10 @@ python_pip "uwsgi" do
   version node['supervisor_django']['uwsgi']['version']
 end
 
+template "#{node.supervisor_django.uwsgi.logdir}" do
+  action :create
+end
+
 template "#{node.supervisor_django.uwsgi.wrapper}" do
   source "uwsgi_django.sh.erb"
   mode "0755"
